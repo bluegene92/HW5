c4175bc2feb474f9b5d9a0870cfc39867b1c8ff0
Dat Tran <dattran10@gmail.com>
2024-04-07 16:02:25 -0500

    Refactor

--- test/storage_test.dart
+++ test/storage_test.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hw4/model/task.dart';
 import 'package:hw4/services/storage.dart';
@@ -9,21 +7,9 @@ import 'mocks/mock_storage.dart';
 
 void main() {
   late Storage mockStorage;
-  late Task task;
-  late List<Task> expectedTasks;
 
   setUp(() {
     mockStorage = MockStorage();
-    task = Task(
-      id: '1',
-      description: 'Sample Task',
-      isCompleted: false,
-    );
-    expectedTasks = [
-      Task(id: '1', description: 'Task 1', isCompleted: false),
-      Task(id: '2', description: 'Task 2', isCompleted: true),
-      Task(id: '3', description: 'Task 3', isCompleted: false),
-    ];
   });
 
   test('Make sure storage is initialized', () async {
